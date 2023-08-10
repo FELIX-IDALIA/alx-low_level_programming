2b25f1259ff0eed11ccbc04389de411e6d9d1794
Felix Idalia <flxidalia@gmail.com>
2023-08-10 14:52:47 +0000

    Modified files: 4-puts.c 5-printf.c 6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * file 4-puts.c
+ */
+
+#include <stdio.h>
+
+/**
+ * The following C program prints "Programming is like building a multilingual
+ *                puzzle
+ *
+ * and returns (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
