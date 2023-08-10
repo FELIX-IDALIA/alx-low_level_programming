304ef009c02ba9fefb084cafcac1bfa7dfd49b70
Felix Idalia <flxidalia@gmail.com>
2023-08-10 15:07:16 +0000

    Modified file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/* 
+ * 5-printf.c
+ */
+
+#include <stdio.h>
+
+/*
+ * main function prints "with proper grammar, but the outcome is a piece of art," 
+ * it is followed by a new line
+ *
+ * returns (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
