bd5838a5bfc38451a2d3401f0e93c2932680f82c
Felix Idalia <flxidalia@gmail.com>
2023-08-21 23:11:25 +0000

    Modified file: 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,10 +3,9 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
+ * main - program starting point
  *
- * Return: Always 0 (Success)
+ * Return: returns zero when success
  */
 int main(void)
 {
