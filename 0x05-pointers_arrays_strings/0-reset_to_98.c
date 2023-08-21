ed4b57ddb8257d4936971520c6c44b962c5c4556
Felix Idalia <flxidalia@gmail.com>
2023-08-21 17:01:49 +0000

    Added files: main.h, _putchar.c 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - starting point
+ *
+ *
+ * @n : represents parameter taken by the function
+ * Return: no return type
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
