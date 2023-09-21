6c8a77853e2f5dca92a9a0f65053c074d4661c5c
Felix Idalia <flxidalia@gmail.com>
2023-09-21 13:46:03 +0000

    File modified; 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - func, frees list_t list
+ * @head: list_t list that need to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *tem;
+
+	while (head)
+	{
+		tem = head->next;
+		free(head->str);
+		free(head);
+		head = tem;
+	}
+}
