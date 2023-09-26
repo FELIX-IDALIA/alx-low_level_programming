ada26a27cd03c4d5197a61447ad1d0187d295a45
Felix Idalia <flxidalia@gmail.com>
2023-09-26 20:20:11 +0000

    File 101-print_listint_safe.c modified

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,15 +9,15 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t number = 0;
-	long int difference;
+	size_t num = 0;
+	long int diff;
 
 	while (head)
 	{
-		difference = head - head->next;
-		number++;
+		diff = head - head->next;
+		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (difference > 0)
+		if (diff > 0)
 			head = head->next;
 		else
 		{
@@ -26,5 +26,5 @@ size_t print_listint_safe(const listint_t *head)
 		}
 	}
 
-	return (number);
+	return (num);
 }
